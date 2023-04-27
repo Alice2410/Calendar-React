62dce8baacb06a87213e1bc76de495b5cca9a0f2
Alice2410 <devo4ka2410@gmail.com>
2023-04-27 21:11:28 +0300

    add styled component for header row

---
+++ src/components/RowWrapper.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const RowWrapper = styled.div`
+  width: 100%;
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: 1fr;
+  margin-bottom: 5px;
+`
