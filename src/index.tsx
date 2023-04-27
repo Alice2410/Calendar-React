e546633ca7aab290bf9a9b571b094464646affa7
Alice2410 <devo4ka2410@gmail.com>
2023-04-27 21:09:41 +0300

    add context provider for app

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import FontStyles from './FontStyles';
+import { CalendarProvider } from './context/CalendarContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <FontStyles />
-    <App />
+    <CalendarProvider>
+      <App />
+    </CalendarProvider>
   </React.StrictMode>
 );
 
